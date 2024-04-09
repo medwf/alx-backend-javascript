d83cb3b1ab947a3b48466f36fc91eca03b80c4e3
med.wf <med.wf95@gmail.com>
2024-04-09 12:34:01 +0000

    add task 10

--- 0x00-ES6_basic/10-loops.js
+++ 0x00-ES6_basic/10-loops.js
@@ -1,1 +1,8 @@
-#!/usr/bin/node
+export default function appendToEachArrayValue(array, appendString) {
+  const New = [];
+  for (const items of array) {
+    New.push(appendString + items);
+  }
+
+  return New;
+}
