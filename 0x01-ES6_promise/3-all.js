639d06e31a0bc137d297a7ddf395ed43854e1124
med.wf <med.wf95@gmail.com>
2024-04-11 15:41:09 +0000

    add promises task 3

--- 0x01-ES6_promise/3-all.js
+++ 0x01-ES6_promise/3-all.js
@@ -1,7 +1,8 @@
 import { uploadPhoto, createUser } from './utils';
 
 export default function handleProfileSignup() {
-  Promise.all([uploadPhoto(), createUser()])
+  return Promise
+    .all([uploadPhoto(), createUser()])
     .then((response) => {
       // console.log(response);
       console.log(`${response[0].body} ${response[1].firstName} ${response[1].lastName}`);
