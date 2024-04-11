f54585343dbfe7b324c9f2c8137f82b55535ffe9
med.wf <med.wf95@gmail.com>
2024-04-11 17:29:35 +0000

    add promises all tasks

--- 0x01-ES6_promise/9-try.js
+++ 0x01-ES6_promise/9-try.js
@@ -1,1 +1,13 @@
-#!/usr/bin/node
+export default function guardrail(mathFunction) {
+  const queue = [];
+
+  try {
+    queue.push(mathFunction());
+  } catch (err) {
+    queue.push(String(err));
+  } finally {
+    queue.push('Guardrail was processed');
+  }
+
+  return queue;
+}
