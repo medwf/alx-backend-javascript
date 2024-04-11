d48f3c3a1831aa62c47f0679a21630c0789c96cd
med.wf <med.wf95@gmail.com>
2024-04-11 17:01:50 +0000

    add promises task 6

--- 0x01-ES6_promise/6-final-user.js
+++ 0x01-ES6_promise/6-final-user.js
@@ -1,1 +1,13 @@
-#!/usr/bin/node
+import signUpUser from './4-user-promise';
+import uploadPhoto from './5-photo-reject';
+
+export default function handleProfileSignup(firstName, lastName, fileName) {
+  return Promise
+    .all([signUpUser(fileName, lastName), uploadPhoto(fileName)])
+    .then((res) => {
+      res.map((obj) => ({
+        status: obj.status,
+        value: obj === 'fulfilled' ? obj.value : String(obj.reason),
+      }));
+    });
+}
