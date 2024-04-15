7fe16131c54b50e20253afd6d28c102569eaad0c
med.wf <med.wf95@gmail.com>
2024-04-15 16:30:45 +0100

    add task 3

--- 0x03-ES6_data_manipulation/3-get_ids_sum.js
+++ 0x03-ES6_data_manipulation/3-get_ids_sum.js
@@ -1,1 +1,8 @@
-#!/usr/bin/node
+export default function getStudentIdsSum(students) {
+  if (students instanceof Array) {
+    return students.reduce(
+      (accumulator, currentid) => accumulator + currentid.id, 0);
+  }
+
+  return 0;
+}
