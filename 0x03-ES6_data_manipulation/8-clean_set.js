4c82824a681aba1858c1beee7187973247f30fe2
med.wf <med.wf95@gmail.com>
2024-04-15 22:01:20 +0100

    add task 8

--- 0x03-ES6_data_manipulation/8-clean_set.js
+++ 0x03-ES6_data_manipulation/8-clean_set.js
@@ -1,1 +1,12 @@
-#!/usr/bin/node
+export default function cleanSet(set, startString) {
+  const text = [];
+  if (set instanceof Set) {
+    for (const value of set) {
+      if (typeof value === 'string' && value.startsWith(startString)
+    && (typeof startString === 'string' && startString.length > 0)) {
+        text.push(value.slice(startString.length));
+      }
+    }
+  }
+  return text.join('-');
+}
