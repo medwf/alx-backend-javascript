bde13ef58d8c79c6bb6f2ef70c5466e0e7759a6e
med.wf <med.wf95@gmail.com>
2024-04-15 17:44:50 +0100

    add task 4

--- 0x03-ES6_data_manipulation/4-update_grade_by_city.js
+++ 0x03-ES6_data_manipulation/4-update_grade_by_city.js
@@ -1,1 +1,20 @@
-#!/usr/bin/node
+export default function updateStudentGradeByCity(getListStudents, city, newGrades) {
+  if (getListStudents instanceof Array
+    && typeof city === 'string'
+    && newGrades instanceof Array
+  ) {
+    return getListStudents.filter(
+      (obj) => obj.location === city,
+    ).map(
+      (obj) => ({
+        id: obj.id,
+        firstName: obj.firstName,
+        location: obj.location,
+        grade: (newGrades.filter(
+          (grade) => grade.studentId === obj.id,
+        ).pop(0) || { grade: 'N/A' }).grade,
+      }),
+    );
+  }
+  return [];
+}
