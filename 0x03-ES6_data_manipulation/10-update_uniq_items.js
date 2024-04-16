5c5aec1837180bc76fd2cf940bf15a36fa090ba5
med.wf <med.wf95@gmail.com>
2024-04-16 13:29:27 +0100

    add task 10

--- 0x03-ES6_data_manipulation/10-update_uniq_items.js
+++ 0x03-ES6_data_manipulation/10-update_uniq_items.js
@@ -1,1 +1,11 @@
-#!/usr/bin/node
+export default function updateUniqueItems(map) {
+  if (!(map instanceof Map)) {
+    throw new TypeError('Cannot process');
+  }
+
+  return map.forEach(
+    (value, key) => (value === 1
+      ? map.set(key, 100)
+      : map.set(key, value)),
+  );
+}
