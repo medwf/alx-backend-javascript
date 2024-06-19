f8825250c87b9f68ef28a926ef9d3020953e32ae
med.wf <med.wf95@gmail.com>
2024-06-19 19:34:44 +0100

    add task 4

--- 0x05-Node_JS_basic/4-http.js
+++ 0x05-Node_JS_basic/4-http.js
@@ -1,11 +1,16 @@
 const http = require('http');
 
-const app = http.createServer((req, res) => {
+const app = http.createServer();
+
+app.on('request', (_, res) => {
   res.statusCode = 200;
   res.setHeader('Content-type', 'text/plain');
-  res.end('Hello Holberton School!');
+  res.setHeader('Content-Length', 23);
+  res.write('Hello Holberton School!');
 });
 
 app.listen(1245, 'localhost', () => {
-  console.log('Server running at http://localhost:1245/*');
+  process.stdout.write('Server running at http://localhost:1245/*\n');
 });
+
+module.exports = app;
