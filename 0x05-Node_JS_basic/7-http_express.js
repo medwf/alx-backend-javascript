79965c50176b7e9b432ee8bad1d135f55e089768
med.wf <med.wf95@gmail.com>
2024-06-20 14:37:00 +0100

    add task 7

---
+++ 0x05-Node_JS_basic/7-http_express.js
@@ -0,0 +1,65 @@
+const express = require('express');
+const fs = require('fs');
+
+const PORT = 1245;
+const nameFile = process.argv.length > 2 ? process.argv[2] : '';
+
+const app = express();
+
+const countStudents = (path) => new Promise((resolve, reject) => {
+  fs.readFile(path, 'utf-8', (err, data) => {
+    if (err) {
+      reject(new Error('Cannot load the database'));
+    }
+
+    let count = 0;
+    const students = {};
+    const response = [];
+    if (data) {
+      const content = data.trim().split('\n');
+
+      for (const lines of content.slice(1)) {
+        count += 1;
+
+        const line = lines.split(',');
+        const field = line[line.length - 1];
+
+        if (!Object.keys(students).includes(field)) {
+          students[field] = [line[0]];
+        } else {
+          students[field].push(line[0]);
+        }
+      }
+    }
+    response.push(`Number of students: ${count}`);
+    for (const [field, value] of Object.entries(students)) {
+      response.push(
+        `Number of students in ${field}: ${value.length}. List: ${value.join(
+          ', ',
+        )}`,
+      );
+    }
+    resolve(response.join('\n'));
+  });
+});
+
+app.get('/', (_, res) => {
+  res.send('Hello Holberton School!');
+});
+
+app.get('/students', (_, res) => {
+  res.appendHeader('Content-type', 'text/plain');
+  countStudents(nameFile)
+    .then((response) => {
+      res.send(`This is the list of our students\n${response}`);
+    })
+    .catch((err) => {
+      res.send(`This is the list of our students\n${err.message}`);
+    });
+});
+
+app.listen(PORT, () => {
+  console.log('Server is running on http://localhost:1245/');
+});
+
+module.exports = app;
