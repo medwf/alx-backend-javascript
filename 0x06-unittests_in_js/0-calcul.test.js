7b4d2e2f38704950449a93a6af78b5377b5d4c67
med.wf <med.wf95@gmail.com>
2024-06-21 16:43:08 +0100

    adding task 0

---
+++ 0x06-unittests_in_js/0-calcul.test.js
@@ -0,0 +1,17 @@
+const assert = require('assert');
+const calculateNumber = require('./0-calcul.js');
+
+describe('calculateNumber', () => {
+  it('a = 1, b = 3 should be 4', () => {
+    assert.strictEqual(calculateNumber(1, 3), 4);
+  });
+  it('a = 1, b = 3.7 should be 5', () => {
+    assert.strictEqual(calculateNumber(1, 3.7), 5);
+  });
+  it('a = 1.2, b = 3.7 should be 5', () => {
+    assert.strictEqual(calculateNumber(1.2, 3.7), 5);
+  });
+  it('a = 1.5, b = 3.7 should be 6', () => {
+    assert.strictEqual(calculateNumber(1.5, 3.7), 6);
+  });
+});
